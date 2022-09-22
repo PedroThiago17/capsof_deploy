3788ebc77ee84eab0d49f36b28d651d2f987f2d9
Pedro Thiago Vergara Vega <n00279437@upn.pe>
2022-09-21 19:47:56 -0500

    cors configuration

--- src/main/java/com/upn/sistemas/capsof_project/CapsofProjectApplication.java
+++ src/main/java/com/upn/sistemas/capsof_project/CapsofProjectApplication.java
@@ -2,12 +2,24 @@ package com.upn.sistemas.capsof_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CapsofProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapsofProjectApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE").maxAge(3600);
+			}
+		};
 	}
 
 }
