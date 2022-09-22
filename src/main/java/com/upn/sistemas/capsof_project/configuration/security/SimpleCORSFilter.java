ccc4bcdd89a0ec92e95fe1d8a1d3ba322695955b
Pedro Thiago Vergara Vega <n00279437@upn.pe>
2022-09-22 09:37:19 -0500

    cors filter

---
+++ src/main/java/com/upn/sistemas/capsof_project/configuration/security/SimpleCORSFilter.java
@@ -0,0 +1,36 @@
+package com.upn.sistemas.capsof_project.configuration.security;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+
+		final HttpServletRequest request = (HttpServletRequest) req;
+		final HttpServletResponse response = (HttpServletResponse) res;
+
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Authorization,Content-Type, Accept, Accept-Language, Currency, Country, X-Requested-With, remember-me");
+
+		chain.doFilter(req, res);
+
+	}
+
+}
