f42a1a24fa5948e92783b123cc871e7dd9dd67a5
Pedro Thiago Vergara Vega <n00279437@upn.pe>
2022-09-22 15:11:23 -0500

    cors validation

--- src/main/java/com/upn/sistemas/capsof_project/configuration/security/SimpleCORSFilter.java
+++ src/main/java/com/upn/sistemas/capsof_project/configuration/security/SimpleCORSFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
@@ -19,10 +18,9 @@ public class SimpleCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 
-		final HttpServletRequest request = (HttpServletRequest) req;
 		final HttpServletResponse response = (HttpServletResponse) res;
 
-		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
 		response.setHeader("Access-Control-Max-Age", "3600");
