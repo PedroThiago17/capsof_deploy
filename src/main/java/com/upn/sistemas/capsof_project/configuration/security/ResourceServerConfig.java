47c21b28dec281f4144ee06a53e29aeaed95ae5c
Pedro Thiago Vergara Vega <pvergarv@everis.nttdata.com>
2022-09-27 20:07:31 -0500

    Modifying filter spring security

--- src/main/java/com/upn/sistemas/capsof_project/configuration/security/ResourceServerConfig.java
+++ src/main/java/com/upn/sistemas/capsof_project/configuration/security/ResourceServerConfig.java
@@ -13,6 +13,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/swagger-ui.html", "/user/addUser").permitAll().anyRequest()
 				.authenticated();
+		http.cors();
+
 	}
 
 }
