f3ea167d7e24f2e2c78637a547949f02b12677c2
Pedro Thiago Vergara Vega <n00279437@upn.pe>
2022-09-28 01:30:48 -0500

    configure cors security 6

--- src/main/java/com/upn/sistemas/capsof_project/configuration/security/SimpleCORSFilter.java
+++ src/main/java/com/upn/sistemas/capsof_project/configuration/security/SimpleCORSFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 
@@ -18,15 +17,15 @@ public class SimpleCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 
-		final HttpServletResponse response = (HttpServletResponse) res;
-
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "*");
-
-		chain.doFilter(req, res);
+//		final HttpServletResponse response = (HttpServletResponse) res;
+//
+//		response.setHeader("Access-Control-Allow-Origin", "*");
+//		response.setHeader("Access-Control-Allow-Credentials", "true");
+//		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+//		response.setHeader("Access-Control-Max-Age", "3600");
+//		response.setHeader("Access-Control-Allow-Headers", "*");
+//
+//		chain.doFilter(req, res);
 
 	}
 }
