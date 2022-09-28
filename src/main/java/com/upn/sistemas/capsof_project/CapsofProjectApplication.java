58bf808673be8fd4024c24f1e39f263ae4b7196e
Pedro Thiago Vergara Vega <n00279437@upn.pe>
2022-09-27 23:54:24 -0500

    configure cors final

--- src/main/java/com/upn/sistemas/capsof_project/CapsofProjectApplication.java
+++ src/main/java/com/upn/sistemas/capsof_project/CapsofProjectApplication.java
@@ -2,26 +2,11 @@ package com.upn.sistemas.capsof_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SuppressWarnings("deprecation")
 @SpringBootApplication
 public class CapsofProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapsofProjectApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
 	}
 }
