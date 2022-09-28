6d770ac0c066632566d4902bf82c78e400c8859d
Pedro Thiago Vergara Vega <n00279437@upn.pe>
2022-09-28 15:48:13 -0500

    cors config resource 2

--- src/main/java/com/upn/sistemas/capsof_project/configuration/security/ResourceServerConfig.java
+++ src/main/java/com/upn/sistemas/capsof_project/configuration/security/ResourceServerConfig.java
@@ -1,18 +1,9 @@
 package com.upn.sistemas.capsof_project.configuration.security;
 
-import java.util.Arrays;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -20,30 +11,29 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/user/addUser", "oauth/token").permitAll().anyRequest().authenticated()
-				.and().cors().configurationSource(corsConfigurationSource());
+		http.authorizeRequests().antMatchers("/user/addUser").permitAll().anyRequest().authenticated().and().cors();
 	}
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("*"));
-		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		config.setAllowCredentials(true);
-		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-		config.addAllowedHeader("access-control-allow-origin");
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-
-		return source;
-	}
-
-	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(
-				new CorsFilter(corsConfigurationSource()));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowedOrigins(Arrays.asList("*"));
+//		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//		config.setAllowCredentials(true);
+//		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+//		config.addAllowedHeader("access-control-allow-origin");
+//
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", config);
+//
+//		return source;
+//	}
+//
+//	@Bean
+//	public FilterRegistrationBean<CorsFilter> corsFilter() {
+//		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(
+//				new CorsFilter(corsConfigurationSource()));
+//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return bean;
+//	}
 }
--- src/main/java/com/upn/sistemas/capsof_project/controller/UserController.java
+++ src/main/java/com/upn/sistemas/capsof_project/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("user")
+@CrossOrigin(origins = "*")
 @Api(tags = "UserApi", value = "/user")
 public class UserController {
 
