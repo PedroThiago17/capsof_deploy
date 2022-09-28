77bf9bbb95756953b850b04c6bf6401c41436324
Pedro Thiago Vergara Vega <n00279437@upn.pe>
2022-09-27 23:37:27 -0500

    configure cors 4

--- src/main/java/com/upn/sistemas/capsof_project/CapsofProjectApplication.java
+++ src/main/java/com/upn/sistemas/capsof_project/CapsofProjectApplication.java
@@ -2,9 +2,6 @@ package com.upn.sistemas.capsof_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class CapsofProjectApplication {
@@ -12,11 +9,4 @@ public class CapsofProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CapsofProjectApplication.class, args);
 	}
-
-	@Bean
-	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-		http.cors(cors -> cors.disable());
-		return http.build();
-	}
-
 }
