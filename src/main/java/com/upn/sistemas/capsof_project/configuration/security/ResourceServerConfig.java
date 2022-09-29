c34900a95f42c3b4b4551d9496a54c30f50c4395
AngieJaramillo <n00279421@upn.pe>
2022-09-28 19:04:46 -0500

    Intento mil two

--- src/main/java/com/upn/sistemas/capsof_project/configuration/security/ResourceServerConfig.java
+++ src/main/java/com/upn/sistemas/capsof_project/configuration/security/ResourceServerConfig.java
@@ -12,6 +12,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/user/addUser").permitAll().anyRequest().authenticated();
-		http.authorizeRequests().antMatchers("/getUserById/{userId}").permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers("/user/getUserById/{userId}").permitAll().anyRequest().authenticated();
 	}
 }
